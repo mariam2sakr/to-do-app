b51db00336ecb01e1bfc5b05ce79113a1f0e79fd
mariam2sakr <mariamsakr2005@gmail.com>
2025-08-17 01:38:28 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const input = document.getElementById("input1")
+const btn = document.getElementById("btn1")
+const list = document.getElementById("tasks-list")
+
+function addtask(){
+    if(input.value ===''){
+        alert("You Must Write Something")
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML=input.value;
+        list.appendChild(li)
+        let span = document.createElement("span");
+        span.innerHTML= "\u00d7" ;
+        li.appendChild(span)
+    }
+    input.value = "" ;
+    saveData()
+}
+
+list.addEventListener("click" , function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false );
+
+function saveData(){
+    localStorage.setItem("data" , list.innerHTML)
+}
+function showTask(){
+    list.innerHTML = localStorage.getItem("data")
+}
+showTask();
